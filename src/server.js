79802e55badffa35feb60f006f862541808577f3
yoonsangjo <131744090+yoonsangjo@users.noreply.github.com>
2023-11-06 11:38:59 +0900

    ## ROUTERS

--- src/server.js
+++ src/server.js
@@ -1,23 +1,19 @@
 import express from 'express';
 import morgan from 'morgan';
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan('dev');
 
-const handleHome = (req, res) => {
-  // return res.send('<h1>I still live you.</h1>');
-  return res.send('I love meddlewares');
-};
-
-const handleLogin = (req, res) => {
-  return res.send('login');
-};
-
 app.use(logger);
-app.get('/', handleHome);
-app.get('/login', handleLogin);
+
+app.use('/', globalRouter);
+app.use('/users', userRouter);
+app.use('/videos', videoRouter);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
