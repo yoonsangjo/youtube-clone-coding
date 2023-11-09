f241c0f937c34d847e6f009a5d79fddc2e90c441
yoonsangjo <131744090+yoonsangjo@users.noreply.github.com>
2023-11-09 15:52:27 +0900

    ## MONGODB AND MONGOOSE - Array Database, Edit Video (post, express.urlencoded)

--- src/server.js
+++ src/server.js
@@ -6,8 +6,6 @@ import videoRouter from './routers/videoRouter';
 
 const PORT = 4000;
 
-console.log(process.cwd());
-
 const app = express();
 const logger = morgan('dev');
 
@@ -15,6 +13,7 @@ app.set('view engine', 'pug');
 app.set('views', process.cwd() + '/src/views');
 
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use('/', globalRouter);
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
