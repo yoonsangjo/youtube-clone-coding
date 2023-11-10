bd97f13923eeba50186ec7f022e9f44544ecb317
yoonsangjo <131744090+yoonsangjo@users.noreply.github.com>
2023-11-10 14:03:07 +0900

    ##  MONGODB AND MONGOOSE - Array Database, Edit video, More Practice, Connecting to Mongo

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+// mongoose.connect('mongodb://127.0.0.1:27017/wetube', {useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect('mongodb://127.0.0.1:27017/wetube');
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log('Connectes to DB');
+const handleError = (error) => console.log('DB Error', error);
+
+db.on('error', handleError);
+db.once('open', handleOpen);
