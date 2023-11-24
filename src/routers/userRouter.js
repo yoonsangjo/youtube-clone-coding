3a6a816e8af0197f943f088f3ab093ac58c96601
yoonsangjo <131744090+yoonsangjo@users.noreply.github.com>
2023-11-24 14:19:03 +0900

    ## USER AUTHENTICATION - Github Login, Logout

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,18 @@
 import express from 'express';
-import { edit, remove, logout, see } from '../controllers/userController';
+import {
+  edit,
+  logout,
+  see,
+  startGithbLogin,
+  finishGithubLogin,
+} from '../controllers/userController';
 
 const userRouter = express.Router();
 
 userRouter.get('/logout', logout);
 userRouter.get('/edit', edit);
-userRouter.get('/remove', remove);
+userRouter.get('/github/start', startGithbLogin);
+userRouter.get('/github/finish', finishGithubLogin);
 userRouter.get(':id', see);
 
 export default userRouter;
