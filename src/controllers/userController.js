5fbe6ce7a198009a01b199cce4aaf9e8ad21db0a
yoonsangjo <131744090+yoonsangjo@users.noreply.github.com>
2023-11-28 15:15:10 +0900

    ## USER PROFILE - Edit Profile Get, Protector and Public Middlewarees, Edit Profile POST, Change Password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -134,5 +134,59 @@ export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect('/');
 };
-export const edit = (req, res) => res.send('Edit');
+export const getEdit = (req, res) => {
+  return res.render('edit-profile', { pageTitle: 'Edit profile' });
+};
+export const postEdit = async (req, res) => {
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { name, email, username, location },
+  } = req;
+  const updateUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      name,
+      email,
+      username,
+      location,
+    },
+    { new: true }
+  );
+  req.session.user = updateUser;
+  return res.redirect('/users/edit');
+};
+
+export const getChangePassword = (req, res) => {
+  if (req.session.user.socialOnly === true) {
+    return res.redirect('/');
+  }
+  return res.render('user/change-password', { pageTitle: 'Change Password' });
+};
+export const postChangePassword = async (req, res) => {
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { oldPassword, newPassword, newPasswordConfirmation },
+  } = req;
+  const user = await User.findById(_id);
+  const ok = await bcrypt.compare(oldPassword, user.password);
+  if (!ok) {
+    return res.status(400).render('user/change-password', {
+      pageTitle: 'Change Password',
+      errorMessage: 'The current password is incorrect',
+    });
+  }
+  if (newPassword !== newPasswordConfirmation) {
+    return res.status(400).render('user/change-password', {
+      pageTitle: 'Change Password',
+      errorMessage: 'The password does not match the confirmation',
+    });
+  }
+  user.password = newPassword;
+  await user.save();
+  return res.redirect('/users/logout');
+};
 export const see = (req, res) => res.send('See User');
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,18 +1,27 @@
 import express from 'express';
 import {
-  edit,
+  getEdit,
+  postEdit,
   logout,
   see,
   startGithbLogin,
   finishGithubLogin,
+  getChangePassword,
+  postChangePassword,
 } from '../controllers/userController';
+import { protectorMiddleware, publicOnlyMiddleware } from '../views/middlewares';
 
 const userRouter = express.Router();
 
-userRouter.get('/logout', logout);
-userRouter.get('/edit', edit);
-userRouter.get('/github/start', startGithbLogin);
-userRouter.get('/github/finish', finishGithubLogin);
+userRouter.get('/logout', protectorMiddleware, logout);
+userRouter.route('/edit').all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter
+  .route('/change-password')
+  .all(protectorMiddleware)
+  .get(getChangePassword)
+  .post(postChangePassword);
+userRouter.get('/github/start', publicOnlyMiddleware, startGithbLogin);
+userRouter.get('/github/finish', publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(':id', see);
 
 export default userRouter;
