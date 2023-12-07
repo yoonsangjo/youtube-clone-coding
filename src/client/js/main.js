c17445ee8b8de5f2dc3058fdf244cf253348b0af
yoonsangjo <131744090+yoonsangjo@users.noreply.github.com>
2023-12-07 17:32:43 +0900

    ## VIDEO PLAYER - Player Setup, Play Pause

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,3 +1,3 @@
 import '../scss/style.scss';
 
-console.log('it works');
+console.log('main');
---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,26 @@
+const video = document.querySelector('video');
+const playBtn = document.getElementById('play');
+const muteBtn = document.getElementById('mute');
+const time = document.getElementById('time');
+const volume = document.getElementById('volume');
+
+const handlePlayClick = (e) => {
+  // if the video is playing, payer it
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  // else play the video
+};
+
+const handlePause = () => (playBtn.innerText = 'Play');
+
+const handlePlay = () => (playBtn.innerText = 'Pause');
+
+const handleMute = (e) => {};
+
+playBtn.addEventListener('click', handlePlayClick);
+muteBtn.addEventListener('click', handleMute);
+video.addEventListener('pause', handlePause);
+video.addEventListener('play', handlePlay);
