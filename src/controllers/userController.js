eeb2e1fbd1ee78045a774250c6b24593a8e901f4
yoonsangjo <131744090+yoonsangjo@users.noreply.github.com>
2023-12-13 17:20:43 +0900

    ## FLASH MESSAGES - installation, Sending Messages

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -135,6 +135,7 @@ export const finishGithubLogin = async (req, res) => {
 };
 
 export const logout = (req, res) => {
+  req.flash('info', 'Bye Bye');
   req.session.destroy();
   return res.redirect('/');
 };
@@ -167,6 +168,7 @@ export const postEdit = async (req, res) => {
 
 export const getChangePassword = (req, res) => {
   if (req.session.user.socialOnly === true) {
+    req.flash('error', "Can't change password.");
     return res.redirect('/');
   }
   return res.render('user/change-password', { pageTitle: 'Change Password' });
@@ -195,6 +197,7 @@ export const postChangePassword = async (req, res) => {
   }
   user.password = newPassword;
   await user.save();
+  req.flash('info', 'Password updated');
   return res.redirect('/users/logout');
 };
 export const see = async (req, res) => {
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -50,12 +50,13 @@ export const postEdit = async (req, res) => {
     user: { _id },
   } = req.session;
   const { title, description, hashtags } = req.body;
-  const video = await Video.exists({ _id: id });
+  const video = await Video.findById(id);
 
   if (!video) {
     return res.status(404).render('404', { pageTitle: 'Video not found.' });
   }
   if (String(video.owner) !== String(_id)) {
+    req.flash('error', 'You are not the owner of the video.');
     return res.status(403).redirect('/');
   }
   await Video.findByIdAndUpdate(id, {
@@ -63,6 +64,7 @@ export const postEdit = async (req, res) => {
     description,
     hashtags: Video.formatHashtags(hashtags),
   });
+  req.flash('success', 'Changes saved.');
   return res.redirect(`/videos/${id}`);
 };
 
